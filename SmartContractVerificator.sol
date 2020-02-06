49a1d56c45c94b42b6288c810fa3498f003f75ee
Chralt98 <chralt98@gmail.com>
2020-02-06 07:56:20 +0100

    added functionality

--- SmartContractVerificator.sol
+++ SmartContractVerificator.sol
@@ -1,10 +1,12 @@
 pragma solidity ^0.6.2;
 
+// owner is the creator of the to verified smart contract
 contract SmartContractVerificator {
+    address constant public programmerVerificator = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
     // only verified programmer ethereum address list
     address[] public registeredProgrammers;
     address payable public wallet;
-    address smartContractOwner;
+    address public smartContractOwner;
     address public smartContractToVerify;
     
     modifier onlyOwner() {
@@ -14,8 +16,7 @@ contract SmartContractVerificator {
     
     modifier onlyVerifiedProgrammer () {
         // check if msg.sender is a verified programmer
-        address[] memory verifiedProgrammers = ProgrammingSkillVerificator(msg.sender).getVerifiedProgrammers();
-        require(verifiedProgrammers[msg.sender].exists, "Your address is not in the verified programmer list.");
+        require(programmerVerificator.isVerified(msg.sender), "Your address is not in the verified programmer list.");
         _;
     }
     
@@ -47,10 +48,24 @@ contract SmartContractVerificator {
     }
 }
 
-contract ProgrammingSkillVerificator {
+contract ProgrammerVerificator {
+    address constant public programmerRiddles = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
     address[] public verifiedProgrammers;
+    mapping(address => uint256[]) public ratings;
+    
+    modifier onlyVerifiedProgrammer () {
+        // check if msg.sender is a verified programmer
+        require(verifiedProgrammers[msg.sender].exists, "Your address is not in the verified programmer list.");
+        _;
+    }
+    
+    function isVerified(address _addr) public returns(bool) {
+        if (verifiedProgrammers[_addr].exists) return true;
+        return false;
+    }
     
     function verifyProgrammer(address _programmer) public {
+        require(!verifiedProgrammers[_programmer].exists, "Programmer is already verified!");
         testProgrammerSkill();
     }
     
@@ -58,9 +73,18 @@ contract ProgrammingSkillVerificator {
         
     }
     
-    function passedTest() internal returns(bool) {
-        // if 100 % of the answers of the questions are true, then the test is passed
-        return true;
+    function isTestPassed(uint256 _score) internal returns(bool) {
+        require(_score >= 0, "Score has to be greater than or equals 0!");
+        require(_score <= 100, "Score has to be smaller than or equals 100!");
+        // if 90 % of the answers are true, then the test is passed
+        if (_score >= 90) return true;
+        return false;
+    }
+    
+    function evaluateProgrammer(address _programmerToEvaluate, uint8 _rating) public onlyVerifiedProgrammer {
+        require(verifiedProgrammers[_programmerToEvaluate].exists, "Specified address is not in the list of verified programmers.");
+        require(2 >= _rating && _rating <= 0, "Specified rating is not between 0 and 2.");
+        ratings[_programmerToEvaluate] = ratings[_programmerToEvaluate].add(_rating);
     }
     
     function getVerifiedProgrammers() public returns (address[] memory) {
@@ -70,6 +94,7 @@ contract ProgrammingSkillVerificator {
 
 contract ProgrammerRiddles {
     address[] public riddles;
+    // let the examinee decide between 3 options of answers
     mapping(string => string) public riddleMapping;
     
     function addRiddle(string memory _question, string memory _answer) public {
@@ -77,3 +102,4 @@ contract ProgrammerRiddles {
         riddleMapping[_question] = _answer;
     }
 }
+
