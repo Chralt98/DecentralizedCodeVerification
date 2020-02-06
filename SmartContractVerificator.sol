069c04dc84dbc42ecf48a30a03704cddd219028a
Chralt98 <chralt98@gmail.com>
2020-02-06 17:16:47 +0100

    added functionality

--- SmartContractVerificator.sol
+++ SmartContractVerificator.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.6.2;
 
+// TODO: list of every verified smart contract should be saved in blockchain
+
 // owner is the creator of the to verified smart contract
 contract SmartContractVerificator {
     address constant public programmerVerificator = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
@@ -16,18 +18,18 @@ contract SmartContractVerificator {
     
     modifier onlyVerifiedProgrammer () {
         // check if msg.sender is a verified programmer
-        require(programmerVerificator.isVerified(msg.sender), "Your address is not in the verified programmer list.");
+        require(programmerVerificator.isVerified(msg.sender), "Your address is not in the verified programmer address list.");
         _;
     }
     
     modifier onlyRegisteredProgrammer () {
         // check if msg.sender is a registered programmer
-        require(registeredProgrammers[msg.sender].exists, "Your address is not in the registered programmer list.");
+        require(registeredProgrammers[msg.sender].exists, "Your address is not in the registered programmer address list.");
         _;
     }
     
     constructor(address _smartContract) public payable {
-        require(isContract(_smartContract), "Specified address is not a smart contract!");
+        require(isContract(_smartContract), "Specified address is not a smart contract! Address should be a smart contract address.");
         wallet.transfer(msg.value);
         smartContractOwner = msg.sender;
         smartContractToVerify = _smartContract;
@@ -49,14 +51,34 @@ contract SmartContractVerificator {
 }
 
 contract ProgrammerVerificator {
-    address constant public programmerRiddles = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
+    address owner;
     address[] public verifiedProgrammers;
-    mapping(address => uint256[]) public ratings;
+    mapping(address => uint8[]) ratings;
+    
+    struct Riddle {
+        string question;
+        string[] possibleAnswers;
+        uint8 correctAnswerIndex;
+    }
+    // let the examinee decide between 3 options of answers
+    mapping(uint8 => Riddle) riddles;
+    
+    uint8 riddleIndex = 0;
     
     modifier onlyVerifiedProgrammer () {
         // check if msg.sender is a verified programmer
-        require(verifiedProgrammers[msg.sender].exists, "Your address is not in the verified programmer list.");
+        require(verifiedProgrammers[msg.sender].exists, "Your address is not in the verified programmer address list.");
         _;
+    }
+    
+    modifier onlyOwner () {
+        // check if msg.sender is a verified programmer
+        require(msg.sender == owner, "Your address is not the owner address!");
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
     }
     
     function isVerified(address _addr) public returns(bool) {
@@ -64,16 +86,22 @@ contract ProgrammerVerificator {
         return false;
     }
     
-    function verifyProgrammer(address _programmer) public {
-        require(!verifiedProgrammers[_programmer].exists, "Programmer is already verified!");
-        testProgrammerSkill();
+    // TODO: question: could multiple address use this function with no mutex problems?
+    // would say yes because i am not working on global variables only with local ones
+    function letMeBeAVerifiedProgrammer() public {
+        require(!verifiedProgrammers[msg.sender].exists, "Your address is already in the verified programmer address list!");
+        require(riddles.size() >= 100, "Not enough riddles have been added by the owner. Wait until owner added at least 100 riddles. Current riddle amount is " + riddles.size());
+        // test the programmer skill with the riddles
+        // first 100 added riddles
+        uint8 _score = 0;
+        // TODO: ask the sender 100 questions with for loop (interact with him)
+        bool _passed = isTestPassed(_score);
+        // TODO: if passed is false the programmer address should get blacklisted for an amount of time
+        require(_passed == true, "Test failed. Your address has not been added to the verified programmer address list.");
+        verifiedProgrammers.add(msg.sender);
     }
     
-    function testProgrammerSkill() internal {
-        
-    }
-    
-    function isTestPassed(uint256 _score) internal returns(bool) {
+    function isTestPassed(uint8 _score) internal returns(bool) {
         require(_score >= 0, "Score has to be greater than or equals 0!");
         require(_score <= 100, "Score has to be smaller than or equals 100!");
         // if 90 % of the answers are true, then the test is passed
@@ -81,6 +109,7 @@ contract ProgrammerVerificator {
         return false;
     }
     
+    // should only be called one time for each test of a programmer
     function evaluateProgrammer(address _programmerToEvaluate, uint8 _rating) public onlyVerifiedProgrammer {
         require(verifiedProgrammers[_programmerToEvaluate].exists, "Specified address is not in the list of verified programmers.");
         require(2 >= _rating && _rating <= 0, "Specified rating is not between 0 and 2.");
@@ -90,16 +119,21 @@ contract ProgrammerVerificator {
     function getVerifiedProgrammers() public returns (address[] memory) {
         return verifiedProgrammers;
     }
-}
-
-contract ProgrammerRiddles {
-    address[] public riddles;
-    // let the examinee decide between 3 options of answers
-    mapping(string => string) public riddleMapping;
     
-    function addRiddle(string memory _question, string memory _answer) public {
-        // make an address which holds the question and answer then add the address to the list
-        riddleMapping[_question] = _answer;
+    // owner could add shuffled version of riddles 
+    function addRiddle(string memory _question, string[] memory _possibleAnswers, uint8 _correctAnswerIndex) public onlyOwner {
+        require(riddleIndex < 100, "Only 100 riddles are needed for the test!");
+        riddles[riddleIndex] = (_question, _possibleAnswers, _correctAnswerIndex);
+        riddleIndex++;
+        // make an address which holds the question and answer then add the address to the list 
+        // transfer this riddle to an IOTA address on the tangle and hold the transaction bundle id of iota here in eth
+        // should not be made with ethereum, riddles should be in the tangle for 24/7 uptime (import the riddles into the ethereum blockchain)
+        // but let the users of ethereum add a riddle, because they use a ETH address and are silly to use IOTA
+    }
+    
+    function deleteRiddles() public onlyOwner {
+        riddles = [];
+        riddleIndex = 0;
     }
 }
 
