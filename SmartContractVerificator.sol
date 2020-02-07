b08a2105830b2448ff12b8c9e4d2b30d7f324f34
Chralt98 <chralt98@gmail.com>
2020-02-07 06:57:11 +0100

    small changes

--- SmartContractVerificator.sol
+++ SmartContractVerificator.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.6.2;
 
 // owner is the creator of the to verified smart contract
 contract SmartContractVerificator {
+    bool internal isVerified = false;
+    
     address constant public programmerVerificator = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
     // only verified programmer ethereum address list
     address[] public registeredProgrammers;
+    
     // wallet which holds the reward for the verificators 
     address payable public wallet;
+    
     address public smartContractOwner;
     address public smartContractToVerify;
     
@@ -34,6 +38,10 @@ contract SmartContractVerificator {
         wallet.transfer(msg.value);
         smartContractOwner = msg.sender;
         smartContractToVerify = _smartContract;
+    }
+    
+    function isSmartContractVerified() public returns (bool) {
+        return isVerified;
     }
     
     function registerForVerification() public onlyVerifiedProgrammer {
