4dac6d55c90814eb5c0883580e263f1de2f3de46
Chralt98 <chralt98@gmail.com>
2020-02-24 17:24:52 +0100

    fixed exists bug for programmer verificator

--- ProgrammerVerificator.sol
+++ ProgrammerVerificator.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.6.2;
 contract ProgrammerVerificator {
     address owner;
     
-    address[] public verifiedProgrammers;
+    mapping(address => bool) verifiedProgrammers;
+    
     mapping(address => uint8) programmerScoreMapping;
     // one address of ratings is one verified programmer
     mapping(address => uint) public ratings;
@@ -12,7 +13,7 @@ contract ProgrammerVerificator {
     uint8 constant MAXIMUM_FAILS = 11;
     mapping(address => uint8) pointMapping;
     mapping(address => uint8) failMapping;
-    mapping(address => bool) isQuestionAllowedMapping;
+    mapping(address => bool) isResolutionAllowedMapping;
     
     struct Riddle {
         string question;
@@ -24,18 +25,13 @@ contract ProgrammerVerificator {
     mapping(uint => Riddle) riddles;
     uint8 riddleIndex = 0;
     
-    modifier onlyVerifiedProgrammer () {
+    modifier onlyVerifiedProgrammer() {
         // check if msg.sender is a verified programmer
-        require(verifiedProgrammers[msg.sender].exists, "Your address is not in the verified programmer address list.");
+        require(verifiedProgrammers[msg.sender], "Your address is not in the verified programmer address list.");
         _;
     }
     
-    modifier onlyRegisteredProgrammer () {
-        require(registeredProgrammers[msg.sender].exists, "Your address is not in the registered programmer address list.");
-        _;
-    }
-    
-    modifier onlyOwner () {
+    modifier onlyOwner() {
         // check if msg.sender is a verified programmer
         require(msg.sender == owner, "Your address is not the owner address!");
         _;
@@ -46,33 +42,33 @@ contract ProgrammerVerificator {
     }
     
     function isProgrammerVerified(address _addr) public returns(bool) {
-        if (verifiedProgrammers[_addr].exists) return true;
+        if (verifiedProgrammers[msg.sender]) return true;
         return false;
     }
     
     // TODO: problem do not get the riddles easy out to the addresses which call this smart contract function to protect the difficulty
     function getRiddle(uint _riddleIndex) public returns(string memory, string[] memory) {
-        require(!verifiedProgrammers[msg.sender].exists, "Your address is already in the verified programmer address list!");
-        require(riddles.size() >= 100, "Not enough riddles have been added by the owner. Wait until owner added at least 100 riddles. Current riddle amount is " + riddles.size());
-        require(!isQuestionAllowedMapping[msg.sender].exists || isQuestionAllowedMapping[msg.sender], "You are not allowed to get a riddle until you solve the last riddle.");
-        isQuestionAllowedMapping[msg.sender] = false;
+        require(!verifiedProgrammers[msg.sender], "Your address is already in the verified programmer address list!");
+        require(riddleIndex >= 100, "Not enough riddles have been added by the owner. Wait until owner added at least 100 riddles.");
+        require(isResolutionAllowedMapping[msg.sender], "You are not allowed to get a riddle until you solve the last riddle.");
+        isResolutionAllowedMapping[msg.sender] = true;
         return (riddles[_riddleIndex].question, riddles[_riddleIndex].possibleAnswers);
     }
     
     // TODO: problem do not get the riddles easy out to the addresses which call this smart contract function to protect the difficulty
     function isRiddleSolved(uint _riddleIndex, uint8 _answerIndex) public returns(bool) {
-        require(!verifiedProgrammers[msg.sender].exists, "Your address is already in the verified programmer address list!");
-        require(riddles.size() >= 100, "Not enough riddles have been added by the owner. Wait until owner added at least 100 riddles. Current riddle amount is " + riddles.size());
+        require(!verifiedProgrammers[msg.sender], "Your address is already in the verified programmer address list!");
+        require(riddleIndex >= 100, "Not enough riddles have been added by the owner. Wait until owner added at least 100 riddles.");
         // TODO for now each failer gets blacklisted forever, do we want this or use a time blacklist ?
-        require(!failMapping[msg.sender].exists || failMapping[msg.sender] < MAXIMUM_FAILS, "Your address reached the maximum of fails for the riddles.");
+        require(failMapping[msg.sender] < MAXIMUM_FAILS, "Your address reached the maximum of fails for the riddles.");
         bool isCorrect = (riddles[_riddleIndex].correctAnswerIndex == _answerIndex);
         if (isCorrect) {
             pointMapping[msg.sender]++;
         } else {
             failMapping[msg.sender]++;
         }
-        if (isTestPassed(pointMapping[msg.sender])) verifiedProgrammers.push(msg.sender);
-        isQuestionAllowedMapping[msg.sender] = true;
+        if (isTestPassed(pointMapping[msg.sender])) verifiedProgrammers[msg.sender] = true;
+        isResolutionAllowedMapping[msg.sender] = false;
         return isCorrect;
     }
     
@@ -88,20 +84,20 @@ contract ProgrammerVerificator {
     // TODO: not sure if the calling Smart contract Verificator calls as onlyVerifiedProgrammer
     // should only be called one time for each test of a programmer
     function evaluateProgrammer(address _programmerToEvaluate, uint8 _rating) public onlyVerifiedProgrammer {
-        require(verifiedProgrammers[_programmerToEvaluate].exists, "Specified address is not in the list of verified programmers.");
+        require(verifiedProgrammers[_programmerToEvaluate], "Specified address is not in the list of verified programmers.");
         require(0 <= _rating && _rating <= 2, "Specified rating is not 0 or 1 or 2, but has to be.");
         // add rating to the verified programmer
         ratings[_programmerToEvaluate] += _rating;
     }
     
-    function getVerifiedProgrammers() public returns (address[] memory) {
+    function getVerifiedProgrammers() public returns (mapping(address => bool) memory) {
         return verifiedProgrammers;
     }
     
     // owner could add shuffled version of riddles 
     function addRiddle(string memory _question, string[] memory _possibleAnswers, uint8 _correctAnswerIndex) public onlyOwner {
         require(riddleIndex < 100, "Only 100 riddles are needed for the test!");
-        require(_possibleAnswers.size() == 3, "There have to be 3 possible answers!");
+        require(_possibleAnswers.length == 3, "There have to be 3 possible answers!");
         require(0 <= _correctAnswerIndex && _correctAnswerIndex <= 2, "The correct answer index should be 0 or 1 or 2 (for 3 possible answers)!");
         riddles[riddleIndex] = Riddle(_question, _possibleAnswers, _correctAnswerIndex);
         riddleIndex++;
@@ -112,11 +108,14 @@ contract ProgrammerVerificator {
         // but let the users of ethereum add a riddle, because they use a ETH address and are silly to use IOTA
     }
     
+    function getRiddlesWithAnswers() public onlyOwner returns(mapping(uint => Riddle) memory) {
+        return riddles;
+    }
+    
     // for refreshing the riddle list that every time another riddle list is created
-    function deleteRiddles() public onlyOwner {
-        // riddles = []
-        delete riddles;
-        riddleIndex = 0;
+    function deleteRiddle(uint _riddleIndex) public onlyOwner {
+        require(_riddleIndex >= 0, "Your riddle index should be greater than zero.");
+        delete riddles[_riddleIndex];
     }
 }
 
