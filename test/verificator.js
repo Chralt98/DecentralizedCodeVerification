cfbf3be27558cc566dd0598b4b6220ca2cb66510
Chralt98 <chralt98@gmail.com>
2020-03-01 21:14:07 +0100

    added msg.sender test for addVerifiedProgrammer

--- test/verificator.js
+++ test/verificator.js
@@ -1,24 +1,41 @@
 const Verificator = artifacts.require("Verificator");
 // accounts = web3.eth.getAccounts();
 contract('Verificator', (accounts) => {
-  it('should add first account as a verified programmer', async () => {
+  it('should add an account as a verified programmer', async () => {
+    // accounts[0] is the owner of the verificator instance
     const verificatorInstance = await Verificator.deployed();
-    const programmer = accounts[0];
+    const programmer = accounts[1];
+    // console.log(verificatorInstance.address);
     await verificatorInstance.addVerifiedProgrammer(programmer).then(async () => {
       const isProgrammerVerified = await verificatorInstance.isProgrammerVerified.call(programmer);
       const programmerPoints = (await verificatorInstance.getVerifiedProgrammerPoints.call(programmer)).toNumber();
-      assert.equal(isProgrammerVerified, true, "First account is not verified.");
+      assert(isProgrammerVerified === true, "First account is not verified.");
       assert.equal(programmerPoints, 10, "Initial account got not the initial 10 points.");
     });
 
   });
-  it('should do this', async () => {
+  it('should revert the transaction of addVerifiedProgrammer when an invalid address calls it', async () => {
     const verificatorInstance = await Verificator.deployed();
+    // here is the owner of the smart contract which deployed it
+    const creatorAddress = accounts[0];
+    const bob = accounts[1];
+    const alice = accounts[2];
 
-    // Setup 2 accounts.
-    const accountOne = accounts[0];
-    const accountTwo = accounts[1];
-    // ...
-    // send coins => await metaCoinInstance.sendCoin(accountTwo, amount, { from: accountOne });
+    return Verificator.deployed()
+      .then(instance => {
+        // bob is not allowed to call addVerifiedProgrammer() to add alice
+        return instance.addVerifiedProgrammer(alice, {from: bob});
+      })
+      // triggers .then() only when the above code works
+      .then(result => {
+        // bob is not allowed to add alice as verified programmer => fails
+        assert.fail();
+      })
+      .catch(error => {
+        // if the above code (fail) runs then the catched error is this assert.fail()
+        // if it is assert.fail() then bob added alice as verified programmer
+        assert.notEqual(error.message, "assert.fail()", "Transaction was not reverted with an invalid address.");
+      });
   });
+  // every integration test in here sees the variables edited above
 });
