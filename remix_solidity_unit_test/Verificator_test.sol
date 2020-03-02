4a8843c4679652226f1487c154aff7bc5481c7e3
Chralt98 <chralt98@gmail.com>
2020-03-02 22:12:09 +0100

    edited comments and gas limit exploration

--- remix_solidity_unit_test/Verificator_test.sol
+++ remix_solidity_unit_test/Verificator_test.sol
@@ -13,15 +13,15 @@ contract MockSmartContractVerificator is Verificator {
 }
 
 contract VerificatorTest {
-  Verificator verificator;
   address verificatorOwner;
+  Verificator ownerVerificator;
 
   /// #sender: account-0
   /// #value: 1000
   function beforeAll() public {
     verificatorOwner = TestsAccounts.getAccount(0);
     compareModifier(msg.sender, verificatorOwner);
-    verificator = new Verificator();
+    ownerVerificator = new Verificator();
   }
 
   function compareModifier(address _sender, address _legitimately) internal {
@@ -32,14 +32,15 @@ contract VerificatorTest {
   /// #value: 1000
   function checkAddingVerifiedProgrammer() public {
     compareModifier(msg.sender, verificatorOwner);
-    address alice = TestsAccounts.getAccount(1);
-    verificator.addVerifiedProgrammer(alice);
-    uint expectedProgrammerPoints = 10;
-    Assert.ok(verificator.isProgrammerVerified(alice), "Programmer should be verified.");
-    Assert.ok(verificator.isProgrammerAllowedToTest(alice), "Programmer should be allowed to test after adding as verified programmer.");
-    Assert.equal(verificator.getVerifiedProgrammerPoints(alice), expectedProgrammerPoints, "Programmer should have 10 points initially after adding as verified programmer.");
 
-    (bool success, bytes memory data) = address(verificator).call.gas(40000).value(0)(abi.encode("addVerifiedProgrammer, [alice]"));
+    address alice = TestsAccounts.getAccount(1);
+    ownerVerificator.addVerifiedProgrammer(alice);
+    uint expectedProgrammerPoints = 10;
+    Assert.ok(ownerVerificator.isProgrammerVerified(alice), "Programmer should be verified.");
+    Assert.ok(ownerVerificator.isProgrammerAllowedToTest(alice), "Programmer should be allowed to test after adding as verified programmer.");
+    Assert.equal(ownerVerificator.getVerifiedProgrammerPoints(alice), expectedProgrammerPoints, "Programmer should have 10 points initially after adding as verified programmer.");
+
+    (bool success, bytes memory data) = address(ownerVerificator).call.gas(40000).value(0)(abi.encode("addVerifiedProgrammer, [alice]"));
     Assert.equal(success, false, "Transaction should revert if address is already verified.");
   }
 
@@ -48,9 +49,10 @@ contract VerificatorTest {
   function checkAddingVerifiedProgrammerNotLegitimate() public returns (bool) {
     // bob is not the owner of the verificator
     compareModifier(msg.sender, TestsAccounts.getAccount(2));
+
     address alice = TestsAccounts.getAccount(1);
-    // check if transacton is correctly reverted
-    (bool success, bytes memory data) = address(verificator).call.gas(40000).value(0)(abi.encode("addVerifiedProgrammer, [alice]"));
+    // alices tries to verify herself which should be reverted correctly
+    (bool success, bytes memory data) = address(ownerVerificator).call.gas(40000).value(0)(abi.encode("addVerifiedProgrammer, [alice]"));
     return Assert.equal(success, false, "Transaction was not reverted with an invalid address.");
   }
 
@@ -58,18 +60,21 @@ contract VerificatorTest {
   /// #value: 1000
   function checkAddingSmartContractVerificator() public {
     compareModifier(msg.sender, verificatorOwner);
+    Verificator verificator = new Verificator();
+
     MockSmartContractVerificator mockSmartContractVerificator = new MockSmartContractVerificator();
     verificator.addSmartContractVerificator(address(mockSmartContractVerificator));
 
     address alice = TestsAccounts.getAccount(1);
     verificator.addVerifiedProgrammer(alice);
-    uint8 positivePoints = 2;
+    Assert.ok(verificator.isProgrammerVerified(alice), "Programmer should be verified.");
 
+    uint8 positivePoints = 2;
     // should only get called by mockSmartContractVerificator
     (bool success, bytes memory data) = address(mockSmartContractVerificator).call.gas(40000).value(0)(abi.encode("addProgrammerPoints, [alice, positivePoints]"));
+    // TODO could be false, because the evm killed transaction because of gas limit
     Assert.equal(success, true, "Transaction should not revert if programmer is verified.");
 
-    /*
     address bob = TestsAccounts.getAccount(2);
 
     // fails if the caller is no smart contract verificator
@@ -79,6 +84,5 @@ contract VerificatorTest {
     // fails if the programmer address is not in the verified programmer list
     (success, data) = address(mockSmartContractVerificator).call.gas(40000).value(0)(abi.encode("addProgrammerPoints, [bob, positivePoints]"));
     Assert.equal(success, false, "Transaction should revert if the programmer is not verified.");
-    */
   }
 }
