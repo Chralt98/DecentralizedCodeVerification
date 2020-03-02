24cf3115fd717cfa0639d20f0a38894a748e90ab
Chralt98 <chralt98@gmail.com>
2020-03-02 19:43:15 +0100

    first experience with gas problems, testing

--- remix_solidity_unit_test/Verificator_test.sol
+++ remix_solidity_unit_test/Verificator_test.sol
@@ -8,10 +8,16 @@ import "remix_accounts.sol";
 // Verificator.sol file should be on the same directory level as this file
 import "../contracts/Verificator.sol";
 
+contract MockSmartContractVerificator is Verificator {
+  constructor() public {}
+}
+
 contract VerificatorTest {
   Verificator verificator;
   address verificatorOwner;
 
+  /// #sender: account-0
+  /// #value: 1000
   function beforeAll() public {
     verificatorOwner = TestsAccounts.getAccount(0);
     compareModifier(msg.sender, verificatorOwner);
@@ -22,6 +28,8 @@ contract VerificatorTest {
     Assert.equal(_sender, _legitimately, "Not allowed to call this function.");
   }
 
+  /// #sender: account-0
+  /// #value: 1000
   function checkAddingVerifiedProgrammer() public {
     compareModifier(msg.sender, verificatorOwner);
     address alice = TestsAccounts.getAccount(1);
@@ -30,9 +38,13 @@ contract VerificatorTest {
     Assert.ok(verificator.isProgrammerVerified(alice), "Programmer should be verified.");
     Assert.ok(verificator.isProgrammerAllowedToTest(alice), "Programmer should be allowed to test after adding as verified programmer.");
     Assert.equal(verificator.getVerifiedProgrammerPoints(alice), expectedProgrammerPoints, "Programmer should have 10 points initially after adding as verified programmer.");
+
+    (bool success, bytes memory data) = address(verificator).call.gas(40000).value(0)(abi.encode("addVerifiedProgrammer, [alice]"));
+    Assert.equal(success, false, "Transaction should revert if address is already verified.");
   }
 
   /// #sender: account-2
+  /// #value: 1000
   function checkAddingVerifiedProgrammerNotLegitimate() public returns (bool) {
     // bob is not the owner of the verificator
     compareModifier(msg.sender, TestsAccounts.getAccount(2));
@@ -40,5 +52,33 @@ contract VerificatorTest {
     // check if transacton is correctly reverted
     (bool success, bytes memory data) = address(verificator).call.gas(40000).value(0)(abi.encode("addVerifiedProgrammer, [alice]"));
     return Assert.equal(success, false, "Transaction was not reverted with an invalid address.");
+  }
+
+  /// #sender: account-0
+  /// #value: 1000
+  function checkAddingSmartContractVerificator() public {
+    compareModifier(msg.sender, verificatorOwner);
+    MockSmartContractVerificator mockSmartContractVerificator = new MockSmartContractVerificator();
+    verificator.addSmartContractVerificator(address(mockSmartContractVerificator));
+
+    address alice = TestsAccounts.getAccount(1);
+    verificator.addVerifiedProgrammer(alice);
+    uint8 positivePoints = 2;
+
+    // should only get called by mockSmartContractVerificator
+    (bool success, bytes memory data) = address(mockSmartContractVerificator).call.gas(40000).value(0)(abi.encode("addProgrammerPoints, [alice, positivePoints]"));
+    Assert.equal(success, true, "Transaction should not revert if programmer is verified.");
+
+    /*
+    address bob = TestsAccounts.getAccount(2);
+
+    // fails if the caller is no smart contract verificator
+    (success, data) = address(bob).call.gas(40000).value(0)(abi.encode("addProgrammerPoints, [alice, positivePoints]"));
+    Assert.equal(success, false, "Transaction should revert if the caller is not a smart contract verificator.");
+
+    // fails if the programmer address is not in the verified programmer list
+    (success, data) = address(mockSmartContractVerificator).call.gas(40000).value(0)(abi.encode("addProgrammerPoints, [bob, positivePoints]"));
+    Assert.equal(success, false, "Transaction should revert if the programmer is not verified.");
+    */
   }
 }
