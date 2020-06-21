691309a83e653cb7dbe92e026772cc9fee4b9465
Chralt98 <chralt98@gmail.com>
2020-06-21 14:05:19 +0200

    server side change event saving (not performance, but works)

--- website/main.py
+++ website/main.py
@@ -10,6 +10,7 @@ from flask_socketio import SocketIO, join_room, leave_room, send, emit
 # pip install pyopenssl
 # from OpenSSL import SSL
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 """
 The flask server application to run the website.
@@ -53,7 +54,9 @@ db.smart_contracts.insert_one(
     {'address': '0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413', 'language': 'Solidity', 'code_lines': 123,
      'state': 'ACTIVE', 'eval_number': 0.65, 'amount': 62})
 
-smart_contracts_code = {'default': ['']}
+# smart contract codes
+# TODO
+db.sc_codes.delete_many({})
 
 
 @app.route('/', defaults={'path': ''})
@@ -87,9 +90,17 @@ def on_join(data):
     room = data['room']
     join_room(room)
     send(username + ' has entered the room.', room=room)
-    string_smart_contracts = '\n'.join(smart_contracts_code[data['room']])
-    print(string_smart_contracts)
-    emit('getCode', string_smart_contracts, broadcast=False)
+    sc_codes = dumps(db.sc_codes.find({'room': room}))
+    if sc_codes == '[]':
+        db.sc_codes.insert_one(
+            {'text': ["pragma solidity ^0.4.0;", "", "contract SimpleStorage {", "    uint storedData;", "",
+                      "    function set(uint x) public {", "        storedData = x;", "    }", "",
+                      "    function get() public view returns (uint) {", "        return storedData;", "    }",
+                      "}"], 'from': {"line": 0, "ch": 0, "sticky": "null"},
+             'to': {"line": 0, "ch": 0, "sticky": "null"}, 'origin': "paste",
+             'room': room})
+        sc_codes = dumps(db.sc_codes.find({'room': room}))
+    emit('getCode', sc_codes, broadcast=False)
 
 
 @socketio.on('leave')
@@ -102,40 +113,60 @@ def on_leave(data):
 
 @socketio.on('connect')
 def on_connect():
-    print('a new user entered')
+    pass
 
 
 @socketio.on('clientText')
 def on_client_text(data):
-    handle_client_text(data['text'], data['from'], data['to'], data['origin'], data['room'])
+    # save_editor_text_on_server(data['text'], data['from'], data['to'], data['origin'], data['room'])
+    db.sc_codes.insert_one(
+        {'text': data['text'], 'from': data['from'], 'to': data['to'], 'origin': data['origin'], 'room': data['room']})
     # only broadcast it to any other then sender
     emit('serverText', data, broadcast=True, room=data['room'], include_self=False)
 
 
-def handle_client_text(text, from1, to1, origin, room):
-    # this counts as selected text from and to is always the selected text
-    # from_char and to_char are equal for paste
+"""
+def save_editor_text_on_server(text, from1, to1, origin, room):
     from_line = from1['line']
     from_char = from1['ch']
     to_line = to1['line']
     to_char = to1['ch']
-    text = text[0]
-    print('FROM_LINE: ' + str(from_line))
-    print('FROM_CHAR: ' + str(from_char))
-    print('TO_LINE: ' + str(to_line))
-    print('TO_CHAR: ' + str(to_char))
-    if from_line == to_line and from_char == to_char:
-        if origin == '+input' and text == '':
-            smart_contracts_code[room].append('')
-        # print one letter
-        smart_contracts_code[room][from_line] = ''.join(
-            replace_str_index(smart_contracts_code[room][from_line], from_char,
-                              text))
-        print(smart_contracts_code[room])
+
+    if len(text) == 2 and text[0] == '' and text[1] == '':
+        handle_line_break(from_line, from_char, room)
+    else:
+        if from_line == to_line:
+            char_list = list(sc_codes[room][from_line])
+            if not char_list:
+                char_list.append('')
+            del char_list[from_char:to_char]
+            if origin == 'paste' or origin == '+input':
+                for i in range(len(text)):
+                    if i == 0:
+                        char_list.insert(from_char, text[i])
+                    else:
+                        sc_codes[room].insert(from_line + i, text[i])
+            sc_codes[room][from_line] = ''.join(char_list)
+        else:
+            for line in range(from_line, to_line + 1):
+                char_list = list(sc_codes[room][line])
+                if line == from_line:
+                    del char_list[from_char:]
+                    if origin == 'paste' or origin == '+input':
+                        char_list.insert(from_char, text[0])
+                elif line == to_line:
+                    del char_list[:to_char]
+                else:
+                    char_list = ['']
+                sc_codes[room][line] = ''.join(char_list)
 
 
-def replace_str_index(text, index1=0, replacement=''):
-    return '%s%s%s' % (text[:index1], replacement, text[index1 + 1:])
+def handle_line_break(from_line, from_char, room):
+    # break line
+    char_list = list(sc_codes[room][from_line])
+    sc_codes[room][from_line] = ''.join(char_list[from_char:])
+    sc_codes[room].insert(from_line, ''.join(char_list[:from_char]))
+"""
 
 
 @app.route('/view/<string:address>', methods=['GET'])
@@ -149,7 +180,8 @@ def view(address):
 def home():
     # get the arguments of the url: request.args.get('name')
     if request.method == 'GET':
-        return render_template('index.html', smart_contracts=db.smart_contracts.find({}))
+        smart_contracts = db.smart_contracts.find({})
+        return render_template('index.html', smart_contracts=smart_contracts)
     elif request.method == 'POST':
         if request.form['searchAddress'] == '':
             return render_template('index.html', smart_contracts=db.smart_contracts.find({}))
